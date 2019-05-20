9b43fa1b4e1e28d6958405e19075d41e1c47d249
ChrisCo31 <christel.coeur@free.fr>
2019-05-20 15:12:07 +0200

    v2

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import Layout2 from '../components/Layout2'
 
-
-
 class Blog extends React.Component {
     render () {
         return (
             <Layout2>
-          
             </Layout2>
             
         )
